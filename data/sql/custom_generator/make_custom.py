52767721b95920e762541d32ffe12b108e02a072
abensoft-joaoluis <joao.santos@abensoft.com.br>
2025-08-01 23:35:32 -0300

    Adds battlemaster

--- data/sql/custom_generator/make_custom.py
+++ data/sql/custom_generator/make_custom.py
@@ -1,6 +1,7 @@
 import os.path
 from pathlib import Path
 
+from modules.MakeCustomNPCS import MakeCustomNPCS
 from modules.CustomSkillSetter import CustomSkillSetter
 
 CUSTOM_WEAPONS = "custom_weapon_proficiencies.sql"
@@ -12,3 +13,4 @@ if __name__ == '__main__':
     CustomSkillSetter().generate_all_weapon_proficiencies_sql(filename=str(Path("../custom/db_world").resolve() / CUSTOM_WEAPONS))
 
     # Custom NPCs
+    MakeCustomNPCS(folder_db=str(Path("../custom/db_characters").resolve())).run()
---
+++ data/sql/custom_generator/modules/MakeCustomNPCS.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+class MakeCustomNPCS:
+    def __init__(self, folder_db):
+        self.filename = Path(folder_db) # Ensure it's a Path object here
+
+    def run(self):
+        self.create_lieutenant_battlemaster()
+
+    def create_lieutenant_battlemaster(self):
+        print("Creating Lieutenant Battlemaster. Location: Northshire Valley")
+
+        with open(self.filename / "battlemaster.sql", "w") as sql_file:
+            sql_file.write("UPDATE acore_world.creature "
+                           "SET position_x = -8913.835, position_y = -135.12477, position_z = 80.4693 WHERE guid = 79942;\n") # Added semicolon and newline
+            sql_file.write("UPDATE acore_world.creature_template "
+                           "SET name = 'Lieutenant Battlemaster' WHERE entry = 823;\n")
+
+            # 4143 = Arathi Vindicator
+            sql_file.write("UPDATE acore_world.creature_template_model "
+                           "SET CreatureDisplayID = 4143 WHERE CreatureID = 823;\n")

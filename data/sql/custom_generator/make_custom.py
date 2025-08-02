e3ef833f5c1a3948dc5e2d871b42a83932883fec
abensoft-joaoluis <joao.santos@abensoft.com.br>
2025-08-02 17:18:13 -0300

    git init dbc files

--- data/sql/custom_generator/make_custom.py
+++ data/sql/custom_generator/make_custom.py
@@ -8,7 +8,6 @@ CUSTOM_WEAPONS = "custom_weapon_proficiencies.sql"
 
 # Creates all custom components and database changes
 if __name__ == '__main__':
-
     # Custom Skills
     CustomSkillSetter().generate_all_weapon_proficiencies_sql(filename=str(Path("../custom/db_world").resolve() / CUSTOM_WEAPONS))
 
